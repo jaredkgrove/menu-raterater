581999712376bd12e2821d6de3f0730989345ad1
Jared Grove <jaredkgrove@gmail.com>
2019-07-31 03:05:30 +0000

    single review per user per menu_item

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,12 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :menu_item
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+  validates_presence_of :unique_user_menu_item_relation?
+
+  def unique_user_menu_item_relation?
+    if self.menu_item.reviews.detect{|review|review.user == self.user}
+    else
+      true
+    end
+  end
 end
