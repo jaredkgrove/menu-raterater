bc7f3d4ab792cdacdc54378798bd8bf9c38388a9
Jared Grove <jaredkgrove@gmail.com>
2019-07-31 14:49:08 +0000

    edit review form update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -31,13 +31,16 @@ class ReviewsController < ApplicationController
     end
 
     get "/:restaurant_slug/menu_items/:menu_item_id/reviews/:id/edit" do
-      @review = Review.find(params[:id]) if !Review.where(id:params[:id]).empty?
+      @review = Review.find(params[:id])
+      @menu_item = MenuItem.find(params[:menu_item_id])
+      @user = @review.user
+      @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
+      redirect to "/#{@review.menu_item.restaurant.slug}/menu_items/#{@review.menu_item.id}/reviews/#{@review.id}" if !(@review.menu_item == @menu_item && @restaurant.menu_items.include?(@menu_item))
+      #could this create infinite loop?
       if !Helpers.logged_in?(session)
         redirect to "/login"
-      elsif !!@review && Helpers.logged_in?(session) && @review.user == Helpers.current_user(session)
+      elsif Helpers.logged_in?(session) && @review.user == Helpers.current_user(session)
         erb :"reviews/edit_review"
-      elsif !!@review
-        redirect to "/#{params[:restaurant_slug]}/menu_items/#{params[:menu_item_id]}/reviews/#{params[:id]}"
       else
         redirect to "/#{params[:restaurant_slug]}/menu_items/#{params[:menu_item_id]}"
       end
@@ -52,14 +55,14 @@ class ReviewsController < ApplicationController
       end
     end
 
-    patch "/reviews/:id" do
+    patch "/:restaurant_slug/menu_items/:menu_item_id/reviews/:id" do
       @review = Review.find(params[:id])
-      # raise params.inspect
       if !params[:rating].empty?
-        @review.update(params) #Check params hash to include rating, comment, menu_item, and ???restaurant???
-        redirect to "/reviews/#{@review.id}"
+        @review.update(rating: params[:rating], comment:params[:comment]) #Check params hash to include rating, comment, menu_item, and ???restaurant???
+        @review.save
+          redirect to "/#{@review.menu_item.restaurant.slug}/menu_items/#{@review.menu_item.id}/reviews/#{@review.id}"
       else
-        redirect to "/reviews/#{@review.id}/edit"
+          redirect to "/#{@review.menu_item.restaurant.slug}/menu_items/#{@review.menu_item.id}/reviews/#{@review.id}/edit"
       end
     end
 
